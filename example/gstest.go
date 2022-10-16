ae27bd5f67aabb299e5fa82b7e3bdd78c1bd46a0
tuxudong <tuxudong@jd.com>
2022-10-16 16:49:38 +0800

    init test

--- example/gstest.go
+++ example/gstest.go
@@ -6,10 +6,24 @@ import (
 	"gorm.io/gorm"
 )
 
+type User struct {
+	ID        uint
+	Username  string
+	FirstName string
+	LastName  string
+}
+
+var users []User = []User{
+	User{Username: "mzuckerberg", FirstName: "Mark", LastName: "Zuckerberg"},
+	User{Username: "bgates", FirstName: "Bill", LastName: "Gates"},
+	User{Username: "sjobs", FirstName: "Steve", LastName: "Jobs"},
+	User{Username: "lellison", FirstName: "Larry", LastName: "Ellison"},
+}
+
 func main() {
 
-	gsCfg := postgres.Config{
-		DSN:                  "host=localhost user=gaussdb password=Enmo@123 dbname=postgres port=15432 sslmode=disable TimeZone=Asia/Shanghai",
+	gsCfg := postgres.Config{ //dbname=postgres
+		DSN:                  "host=localhost user=gaussdb password=Enmo@123 port=15432 sslmode=disable TimeZone=Asia/Shanghai",
 		PreferSimpleProtocol: true,
 	}
 
