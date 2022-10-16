5705bfbe82d0fd52cd2a8cefd3579cd432f5aa34
tuxudong <tuxudong@jd.com>
2022-10-16 16:24:01 +0800

    init test

---
+++ example/README.md
@@ -0,0 +1,13 @@
+# 验证GORM PostgreSQL Driver适配OpenGauss
+
+## 准备环境
+
+### 启一个opengauss db实例
+```shell script
+docker run --name opengauss --privileged=true -d -e GS_PASSWORD=Enmo@123 -p 15432:5432 jdcloudiaas/turta:gaussdb 
+```
+
+## 测试代码
+```shell script
+go run gstest.go
+```
---
+++ example/gstest.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+)
+
+func main() {
+
+	gsCfg := postgres.Config{
+		DSN:                  "host=localhost user=gaussdb password=Enmo@123 dbname=gaussdb port=15432 sslmode=disable TimeZone=Asia/Shanghai",
+		PreferSimpleProtocol: true,
+	}
+
+	gsDialector := postgres.New(gsCfg)
+
+	gsdb, err := gorm.Open(gsDialector, &gorm.Config{})
+	if err != nil {
+		fmt.Println("Failed to connect opengauss db %v", err)
+		return
+	}
+
+	if gsdb != nil {
+		fmt.Println("DB struct is %v", gsdb)
+	}
+
+}
