6f0bc3b436759b5a07ca1b91056b5356850f0a5d
tuxudong <tuxudong@jd.com>
2022-10-16 18:26:27 +0800

    to check gorm postgres driver for gaussdb

--- example/README.md
+++ example/README.md
@@ -1,14 +1,25 @@
-# 验证GORM PostgreSQL Driver适配OpenGauss
+# GORM PostgreSQL Driver适配OpenGauss
 
 ## 准备环境
 
 ### 启一个opengauss db实例
 ```shell script
+# 起实例
 docker run --name opengauss --privileged=true -d -e GS_PASSWORD=Enmo@123 -p 15432:5432 enmotech/opengauss:3.0.0 
+
+# 创数据库
+docker ps | grep opengauss # get container id
+docker cp school.sql $container_id:/
+
+docker exec -ti $container_id bash 
+su - omm
+/usr/local/opengauss/bin/gsql -f /school.sql  # create school db 
+
 ```
 
 ## 测试代码
 ```shell script
 go mod tidy
 go run gstest.go
+
 ```
--- example/gstest.go
+++ example/gstest.go
@@ -6,6 +6,18 @@ import (
 	"gorm.io/gorm"
 )
 
+// 对照school.sql中的定义
+/*
+-- 创建表user
+CREATE TABLE "user"
+(
+    id INT PRIMARY KEY,
+    username VARCHAR(50),
+    first_name VARCHAR(20),
+    last_name  VARCHAR(30)
+);
+*/
+
 type User struct {
 	ID        uint
 	Username  string
@@ -22,27 +34,44 @@ var users []User = []User{
 
 func main() {
 
-	gsCfg := postgres.Config{ //dbname=postgres
-		DSN:                  "host=localhost user=gaussdb password=Enmo@123 port=15432 sslmode=disable TimeZone=Asia/Shanghai",
+	gsCfg := postgres.Config{ //dbname=gaussdb # 默认
+		DSN:                  "host=localhost user=gaussdb password=Enmo@123 dbname=school port=15432 sslmode=disable TimeZone=Asia/Shanghai",
 		PreferSimpleProtocol: true,
 	}
 
 	gsDialector := postgres.New(gsCfg)
 
-	postgresDB, err := gorm.Open(gsDialector, &gorm.Config{})
+	db, err := gorm.Open(gsDialector, &gorm.Config{})
 	if err != nil {
-		fmt.Println("Failed to connect postgresDB db %v", err)
+		fmt.Errorf(fmt.Sprintf("Failed to connect postgresDB db %v", err))
 		return
 	}
 
-	if postgresDB == nil {
-		fmt.Println("DB instance is null")
-		return
+	db.AutoMigrate(&User{})
+
+	for _, user := range users {
+		db.Create(&user)
 	}
 
-	if testDB := postgresDB.Exec("CREATE DATABASE testdb"); testDB.Error != nil {
-		fmt.Println("create databse test01db failed, error=%v", testDB.Error)
-		return
+	var users = make([]User, 0)
+
+	result := db.Find(&users)
+	if result.Error == nil {
+		fmt.Println(fmt.Sprintf("[before] total count is %d", result.RowsAffected))
+	} else {
+		fmt.Errorf(fmt.Sprintf("[before] query failed by error %v", result.Error))
 	}
 
+	// Delete incoming data with filter
+	db.Where(&User{Username: "bgates"}).Delete(&User{})
+
+	users = make([]User, 0)
+	result = db.Find(&users)
+	if result.Error == nil {
+		fmt.Println(fmt.Sprintf("[after] total count is %d", result.RowsAffected))
+	} else {
+		fmt.Errorf(fmt.Sprintf("[after] query failed by error %v", result.Error))
+	}
+
+	fmt.Println("done.")
 }
